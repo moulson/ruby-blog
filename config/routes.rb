0589b20f3c1b53133e1068c643103d03e1d56ade
Daniel Moulson <dan24797@gmail.com>
2019-09-17 10:10:35 +0100

    added basic auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'home#index'
+
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'session#destroy', as: 'logout'
 end
